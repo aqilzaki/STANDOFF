65703c90b8f097a8fce76e315abec3682ba60654
aqil zaki <132360038+aqilzaki@users.noreply.github.com>
2026-09-02 14:29:48 +0700

    Initial Commit

---
+++ aqil.game/aqil_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  aqil_gameApp.swift
+//  aqil.game
+//
+//  Created by muhammad aqil zaki on 02/09/26.
+//
+
+import SwiftUI
+
+@main
+struct aqil_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
